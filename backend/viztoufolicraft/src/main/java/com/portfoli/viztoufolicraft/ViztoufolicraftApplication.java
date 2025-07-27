4acf6ad98bd1f1c74132beecfbe3b4e17ffac6f2
Mind freezer <116938255+vizahat36@users.noreply.github.com>
2025-07-27 16:23:34 +0530

    successfully created frontend and backend folder

---
+++ backend/viztoufolicraft/src/main/java/com/portfoli/viztoufolicraft/ViztoufolicraftApplication.java
@@ -0,0 +1,13 @@
+package com.portfoli.viztoufolicraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViztoufolicraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViztoufolicraftApplication.class, args);
+	}
+
+}
